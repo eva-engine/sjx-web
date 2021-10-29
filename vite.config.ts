aaf4294f3147aff03efb7f5597864e6e89250ebc
aspsnd <2546697613@qq.com>
2021-10-29 16:07:22 +0800

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true
+  },
+  base: "/sjx/",
+  root: './',
+  publicDir: "./public",
   plugins: [react()]
 })
